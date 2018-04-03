b0e0fdfaece6eac3c2de9c584e9c9c6e2888909f
Isaac Ezer <isaac.ezer@gmail.com>
2018-04-03 18:37:17 -0400

    add title

--- web/views/event_view.ex
+++ web/views/event_view.ex
@@ -1,6 +1,6 @@
 defmodule Cats.EventView do
   use JaSerializer.PhoenixView
 
-  attributes [:name, :url, :start_time, :end_time]
+  attributes [:title, :url, :start_time, :end_time]
   has_many :artists, serializer: Cats.ArtistView
 end
