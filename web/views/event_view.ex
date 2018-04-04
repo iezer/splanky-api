ed55750fa96d57acd5fd0df53c2df0143d4e3c91
Isaac Ezer <isaac.ezer@gmail.com>
2018-04-03 23:32:36 -0400

    add artist_ids for backwards compatibility

--- web/views/event_view.ex
+++ web/views/event_view.ex
@@ -1,6 +1,6 @@
 defmodule Cats.EventView do
   use JaSerializer.PhoenixView
 
-  attributes [:title, :url, :start_time, :end_time]
+  attributes [:title, :url, :start_time, :end_time, :artist_ids]
   has_many :artists, serializer: Cats.ArtistView
 end
